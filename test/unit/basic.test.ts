dbd83ea0be48ef2fa04d46b751c889d072319e93
Yoshi Jaeger <github@jaeger.berlin>
2024-11-07 21:01:41 +0100

    test: added unit test that sends log messages using SyslogClient and receives them using test-bound listening sockets (tcp/udp)

---
+++ test/unit/basic.test.ts
@@ -0,0 +1,139 @@
+/**
+ * Unit tests for the action's entrypoint, src/index.ts
+ */
+
+import dgram from 'node:dgram';
+import net from 'node:net';
+import { EventEmitter } from 'node:events';
+
+import Debug from 'debug';
+const debug = Debug('test:unit');
+
+import {
+  Facility,
+  IIdentity,
+  Severity,
+  SyslogClient,
+} from '../../src/index.js';
+
+const DEFAULT_IDENTITY: IIdentity = {
+  facility: Facility.LOCAL0,
+  severity: Severity.INFORMATIONAL,
+  appName: 'TestApp',
+  syslogHostname: 'test-host',
+};
+
+const UDP_PORT = 41234;
+const TCP_PORT = 41235;
+
+describe('SyslogClient', () => {
+  it('should allow construction of the SyslogClient class', () => {
+    const client = new SyslogClient({
+      hostname: 'localhost',
+      port: 514, 
+      transport: 'udp',
+    }, DEFAULT_IDENTITY);
+  });
+
+  describe('UDP Connection', () => {
+    let server: dgram.Socket;
+    let port = UDP_PORT;
+
+    beforeAll((done) => {
+      server = dgram.createSocket('udp4');
+
+      server.on('error', (err) => {
+        debug(`Server error:\n${err.stack}`);
+        server.close();
+      });
+
+      server.on('listening', () => {
+        const address = server.address();
+        debug(`Server listening ${address.address}:${address.port}`);
+        done();
+      });
+
+      server.bind(port);
+    });      
+
+    afterAll((done) => {
+      server.close();
+      done();
+    });    
+
+    it('should allow udp message transmission', (done) => {
+      const client = new SyslogClient({
+        hostname: '127.0.0.1',
+        port: UDP_PORT, 
+        transport: 'udp',
+      }, DEFAULT_IDENTITY);
+
+      server.once('message', (msg, rinfo) => {
+        debug(`Server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+
+        expect(msg.toString()).toContain('Hello UDP');
+
+        client.disconnect();
+
+        done();
+      });
+
+      client.connect().then(async () => {
+        await client.log('Hello UDP');
+      });
+    });
+  });
+
+  describe('TCP Connection', () => {
+    let server: net.Server;
+    let port = TCP_PORT;
+
+    let bridge = new EventEmitter();
+
+    beforeAll((done) => {
+      server = net.createServer(socket => {
+        socket.on('data', (data) => {
+          debug('Data received from client:', data.toString());
+          bridge.emit('message', data.toString(), socket);
+        }); 
+      });
+
+      server.on('error', (err) => {
+        debug(`Server error:\n${err.stack}`);
+        server.close();
+      });
+
+      server.listen(port, () => {
+        debug(`Server listening on port ${port}`);
+        done();
+      });
+    });      
+
+    afterAll((done) => {
+      server.close();
+      done();
+    });    
+
+    it('should allow tcp message transmission', (done) => {
+      const client = new SyslogClient({
+        hostname: '127.0.0.1',
+        port: TCP_PORT, 
+        transport: 'tcp',
+      }, DEFAULT_IDENTITY);
+
+      bridge.once('message', (msg, rinfo) => {
+        debug(`Server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+
+        expect(msg.toString()).toContain('Hello TCP');
+
+        client.disconnect();
+
+        done();
+      });
+
+      client.connect().then(async () => {
+        await client.log('Hello TCP');
+      });
+    });
+  });  
+});
